4fc4437b1d03c7c15fb893265416cf82cf45b0c1
Calvin Thomas Dani <calvinthomas.dani@gmail.com>
2023-06-18 16:56:48 -0700

    remove cors for main code

--- blockonomics-analytics-server/analytics_server.py
+++ blockonomics-analytics-server/analytics_server.py
@@ -2,29 +2,29 @@
 #In this case taking the message with the highest donation
 # - get_message_max_value queries the mongoDb and returns the 
 #       message with the highest donation 
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify, Response
 import os
 import configparser
 from db_analytics import get_comment_largest_value
 from pymongo import MongoClient
 from flask_cors import CORS, cross_origin
 
-ALLOWED_ORIGINS = ['localhost', '127.0.0.1','https://crypto-billboard.onrender.com']
+# ALLOWED_ORIG/INS = ['localhost', '127.0.0.1','https://crypto-billboard.onrender.com','http://localhost:3000/']
 
 analytics_app = Flask(__name__)
 analytics_app.config['CORS_HEADERS'] = "Content-Type"
 
-cors = CORS(analytics_app, resources={"/*": {"origins": ALLOWED_ORIGINS}})
+# cors = CORS(analytics_app)
 
-config = configparser.ConfigParser()
-config.read(os.path.abspath(os.path.join(".ini")))
+# config = configparser.ConfigParser()
+# config.read(os.path.abspath(os.path.join(".ini")))
 
 result = None
 emptyResponse = {"message":"","name":""}
 
 # Get max valued message
+# @cross_origin()
 @analytics_app.route("/get_message",methods=["GET"])
-@cross_origin()
 def get_message_max_value():
     with analytics_app.app_context():
         result = get_comment_largest_value()
@@ -38,4 +38,4 @@ if __name__ == '__main__':
     analytics_app.config['DEBUG'] = False
     analytics_app.config['MONGO_URI'] = os.getenv('DB_URI')
     print(os.getenv('DB_URI'),os.getenv('PORT_ANALYTICS'))
-    analytics_app.run(debug=False, port=os.getenv('PORT_ANALYTICS'))
+    analytics_app.run(debug=False, port=5000)
