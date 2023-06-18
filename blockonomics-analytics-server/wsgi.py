4fcf4398649367de18834cbf47e611ef2adb7161
Calvin Thomas Dani <calvinthomas.dani@gmail.com>
2023-06-18 02:36:37 -0700

    Meta data added to wsgi

--- blockonomics-analytics-server/wsgi.py
+++ blockonomics-analytics-server/wsgi.py
@@ -1,4 +1,6 @@
 from analytics_server import analytics_app
 import os
 if __name__ == "__main__":
+    analytics_app.config['DEBUG'] = False
+    analytics_app.config['MONGO_URI'] = os.getenv('DB_URI')
     analytics_app.run(debug=False,host='0.0.0.0', port=os.getenv('PORT_ANALYTICS'))
