57f754ac32b7a7eb5cee60a51635a8dd73099a5f
Calvin Thomas Dani <calvinthomas.dani@gmail.com>
2023-06-18 02:00:29 -0700

    Env analytics

--- blockonomics-analytics-server/analytics_server.py
+++ blockonomics-analytics-server/analytics_server.py
@@ -37,4 +37,4 @@ if __name__ == '__main__':
     # run app in on port 5005
     analytics_app.config['DEBUG'] = False
     analytics_app.config['MONGO_URI'] = os.getenv('DB_URI')
-    analytics_app.run(debug=False, port=os.getenv('PORT'))
+    analytics_app.run(debug=False, port=os.getenv('PORT_ANALYTICS'))
--- blockonomics-analytics-server/wsgi.py
+++ blockonomics-analytics-server/wsgi.py
@@ -1,3 +1,4 @@
 from analytics_server import analytics_app
+import os
 if __name__ == "__main__":
-    analytics_app.run()
+    analytics_app.run(debug=False,host='0.0.0.0', port=os.getenv('PORT_ANALYTICS'))
