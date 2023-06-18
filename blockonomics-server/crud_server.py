bd22fd62f72d3121efe7438ac295a13d00d9dadd
Calvin Thomas Dani <calvinthomas.dani@gmail.com>
2023-06-18 01:52:09 -0700

    envvar change

--- blockonomics-server/crud_server.py
+++ blockonomics-server/crud_server.py
@@ -43,4 +43,4 @@ if __name__ == '__main__':
     app1.config['DEBUG'] = False
     app1.config['MONGO_URI'] = os.getenv('DB_URI')
 
-    app1.run(debug=False, port=os.getenv('PORT'))
+    app1.run(debug=False,host='0.0.0.0', port=os.getenv('PORT_CRUD'))
--- blockonomics-server/wsgi.py
+++ blockonomics-server/wsgi.py
@@ -1,3 +1,5 @@
 from crud_server import app1
+import os
+
 if __name__ == "__main__":
-    app1.run()
+    app1.run(debug=False,host='0.0.0.0', port=os.getenv('PORT_CRUD'))
